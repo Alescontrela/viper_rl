5655192df6438fabb29a65ae3e0f12bc91154690
Ademi Adeniji <ademi@stanford.edu>
2023-06-22 18:17:07 -0700

    add clas conditional log prob

--- viper_rl/videogpt/models/videogpt.py
+++ viper_rl/videogpt/models/videogpt.py
@@ -43,12 +43,16 @@ class VideoGPT(nn.Module):
             deterministic=not training,
         )
 
-    def log_prob(self, embeddings, encodings, label=None, training=False, reduce_sum=True):
-        logits = self(embeddings, label=label, training=training)
+    def log_prob(self, embeddings, encodings, label=None, text=None, text_mask=None, training=False, reduce_sum=True):
+        logits = self(embeddings, label=label, text=text, text_mask=text_mask, training=training)
         labels = jax.nn.one_hot(encodings, self.ae.n_embed)
         nll = optax.softmax_cross_entropy(logits, labels)
-        if reduce_sum:
-            nll = nll.reshape(*nll.shape[:2], -1).sum(-1)
+        if self.config.class_cond:
+            nll = nll.reshape(*nll.shape[:2], -1)
+            nll = (nll.max(-1) * np.prod(encodings.shape[2:]) + nll.sum(-1)) / (2 * np.prod(encodings.shape[2:]))
+        else:
+            if reduce_sum:
+                nll = nll.reshape(*nll.shape[:2], -1).sum(-1)
         return -nll
 
     def loss(self, embeddings, encodings, label=None, training=True):
